de661860884c3a8ecaff2935d6115b9f473b17aa
tatarynrm <tatarynrm@gmail.com>
2023-07-14 14:34:56 +0300

    git pull

--- index.js
+++ index.js
@@ -34,12 +34,11 @@ const eventsRoutes = require("./routes/events");
 
 // Middlewares------------------------------------------------------------------------------------------------------
 
-app.use(cors({
-  origin:"https://noris-dev.space"
-}));
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json());
+app.use(cors());
 // Відключає CORS
 
 // Відключає CORS
