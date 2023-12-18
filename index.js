dd85d1f7f989f53390a28f5b0ec3ca9db45f746a
tatarynrm <tatarynrm@gmail.com>
2023-12-18 12:52:03 +0200

    ewqewqqew

--- index.js
+++ index.js
@@ -131,11 +131,11 @@ io.on("connection", (socket) => {
     io.emit("showNewZap", data);
     // // БОТ
 // console.log(data);
-    // if (data.pZapCina === 1) {
-    //   sendMessageToGroupZapCina(bot, data);
-    // } else {
-    //   sendMessageToGroup(bot, data);
-    // }
+    if (data.pZapCina === 1) {
+      sendMessageToGroupZapCina(bot, data);
+    } else {
+      sendMessageToGroup(bot, data);
+    }
   });
   socket.on("deleteZap", (data) => {
     io.emit("deleteZapAllUsers", data);
