d9dfbfea741eed14f2fccc3dd5e26bbff338df4e
tatarynrm <tatarynrm@gmail.com>
2023-12-26 16:28:47 +0200

    ewqweqeqw

--- controllers/emails.js
+++ controllers/emails.js
@@ -1,30 +1,108 @@
 const oracledb = require("oracledb");
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-// const pool = require("../db/index");
 const pool = require("../db/pool");
+
 const {
-  GET_ALL_USERS,
-  GET_ALL_ACTIVE_USERS,
-  GET_ALL_FIRED_USERS,
-} = require("../queries/user");
-const { sendNewYearEmail } = require("../nodemailer/newYearNodemailer");
+  sendNewYearEmail,
+  sendEmail,
+} = require("../nodemailer/newYearNodemailer");
+const norisdb = require("../db/noris/noris");
 // const { sendBuhTransport } = require("../index");
 
 const sendNewYearEmailFunction = async (req, res) => {
-
-    const {text} = req.body
-    console.log(text);
+  const { text } = req.body;
+  const connection = await oracledb.getConnection(pool);
+  connection.currentSchema = "ICTDAT";
   try {
-sendNewYearEmail(text)
+    console.log(text);
+
+    const result = await connection.execute(`
+    select a.kod as kod_kontakt,
+           b.kod,
+           a.kod_ur,
+           c.drcode,
+           a.nkontakt,
+           a.prim,
+           b.val,
+           c.ntype,
+           e.nur,
+           e.perekmt,
+           e.peradr,
+           e.pernegabarit,
+           f.nkraina,
+           g.nobl
+    from kontakt a
+    left join kontaktval b on a.kod = b.kod_kontakt
+    left join kontakttype c on b.kod_type = c.kod
+    join ur e on a.kod_ur = e.kod
+    left join kraina f on e.kod_kraina = f.kod
+    left join obl g on e.kod_obl = g.kod
+    where c.drcode = 'EMAIL' and
+          exists (select * from zaylst u where (u.kod_zam = a.kod_ur or u.kod_per = a.kod_ur) and u.perevdat >= to_date('01.01.2023','dd.mm.yyyy'))
+    `)
+console.log(result.rows);
+
+
+    const myEmails = [
+      "lembergus@gmail.com",
+      "rt@ict.lviv.ua",
+      "ab@ict.lviv.ua",
+      "ab.ict.lviv@gmail.com",
+      "tatarynrm@gmail.com",
+      "bolix@ex.ua",
+    ];
+    sendEmail('rt@ict.lviv.ua',text)
+    function scheduleEmails() {
+      // const emails = []; // Ваш масив із 4000 електронних адрес
+      // const myEmails = [
+      //   "lembergus@gmail.com",
+      //   "rt@ict.lviv.ua",
+      //   "ab@ict.lviv.ua",
+      //   "ab.ict.lviv@gmail.com",
+      //   "tatarynrm@gmail.com",
+      //   "bolix@ex.ua",
+      // ];
+      const myEmails = [
+        "johqewjhoqwehoeqwhuiewqujih",
+        "rt@ict.lviv.ua",
+        'ab@ict.lviv.ua'
+
+      ];
+      const twenty_minutes_interval = 20 * 60 * 1000;
+      let index = 0;
+      const interval = setInterval(() => {
+        // const batch = emails.slice(index, index + 100);
+        const batch = myEmails.slice(index, index + 2);
+  
+        batch.forEach((email) => {
+          console.log(`Надіслано привітання ${email}`);
+          sendEmail(email,text);
+          const newFeedBack =  norisdb.query(
+            `
+             INSERT INTO emails (email)
+             values ('${email}')
+             returning *
+             `
+          );
+        });
+
+        // index += 100;
+        index += 2; 
+
+        if (index >= myEmails.length) {
+          clearInterval(interval); // Зупинити інтервал після відправлення всіх листів
+          console.log('Усі листи відправлено');
+        }
+      }, 5000); // Відправляти кожні 10 хвилин
+    }
+    if (text) {
+      scheduleEmails();
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
-
-
-
 module.exports = {
-    sendNewYearEmailFunction
-
+  sendNewYearEmailFunction,
 };
--- nodemailer/newYearNodemailer.js
+++ nodemailer/newYearNodemailer.js
@@ -14,10 +14,20 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-      user: "ict-info-logistics@ict.lviv.ua",
-      pass: "Tfc34#sR51",
+      user: "noreply@ict.lviv.ua",
+      pass: "Tgb34#vC8u",
     },
   });
+// const transporter = nodemailer.createTransport({
+//     // service: "smtp",
+//     host: "mail.ict.lviv.ua",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: "ict-info-logistics@ict.lviv.ua",
+//       pass: "Tfc34#sR51",
+//     },
+//   });
 // Створіть об'єкт з параметрами листа
 const handlebarOptions = {
   viewEngine: {
@@ -50,12 +60,12 @@ transporter.use(
 //     console.log(`Лист відправлено: ${info.response}`);
 //   }
 // });
-const urlImg = 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/08/image-url-3.jpg'
+// const urlImg = 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/08/image-url-3.jpg'
 const sendNewYearEmail = async (text) => {
     const mailOptions = {
-        from: 'ict-info-logistics@ict.lviv.ua',
+        from: 'noreply@ict.lviv.ua',
         subject: 'ДТЕП ІСТ-Захід вітає вас з Новим Роком!',
-        text: "ok",
+        // text: "ok",
         template:"email",
               context: {
         text:text,
@@ -78,7 +88,52 @@ const sendNewYearEmail = async (text) => {
 const pathImage = ()=>{
     console.log(base64String);
 }
+
+// Функція для відправлення листа
+function sendEmail(toEmail,text) {
+  const mailOptions = {
+    from: 'noreply@ict.lviv.ua', // Ваша електронна адреса
+    to: toEmail,
+    subject: 'ДТЕП ІСТ-Захід вітає вас з Новим Роком!',
+    template:"email",
+    context: {
+text:text,
+img:'https://api.ict.lviv.ua/files/new-year.jpeg',
+},
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error.message);
+    } else {
+      console.log(`Email sent: ${info.response}`);
+    }
+  });
+}
+
+// Функція для розсилки листів кожні 10 хвилин
+function scheduleEmails() {
+  const emails = []; // Ваш масив із 4000 електронних адрес
+
+  let index = 0;
+  const interval = setInterval(() => {
+    const batch = emails.slice(index, index + 150);
+    batch.forEach((email) => {
+      sendEmail(email);
+    });
+
+    index += 150;
+
+    if (index >= emails.length) {
+      clearInterval(interval); // Зупинити інтервал після відправлення всіх листів
+    }
+  }, 10 * 60 * 1000); // Відправляти кожні 10 хвилин
+}
+
+
 module.exports = {
     sendNewYearEmail,
-  pathImage
+  pathImage,
+  scheduleEmails,
+  sendEmail
 };
