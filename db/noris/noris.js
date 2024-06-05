e9f2cb088363746ac45c9a2a60e206de53559c29
tatarynrm <tatarynrm@gmail.com>
2024-06-05 16:49:26 +0300

    ewqe

--- db/noris/noris.js
+++ db/noris/noris.js
@@ -9,10 +9,14 @@ const norisdb = new Pool({
 });
 const ictmainsite = new Pool({
   user: "postgres",
-  // host: "185.25.117.64",
-  host: "localhost",
+  host: "185.25.117.64",
+// LOCAL
+  // host: "localhost",
+  // password: "Aa527465182",
+//LOCAL
   database: "ict_main_site",
-  password: "Aa527465182",
+  password: "noris",
+
   port: 5432, // PostgreSQL default port
 });
 
