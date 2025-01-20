418bc016abb347323653d0dd7e2e6d8fc358b617
tatarynrm <tatarynrm@gmail.com>
2025-01-21 01:06:36 +0200

    ewqewq

--- routes/mobile-app/notifications.js
+++ routes/mobile-app/notifications.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 
-const { saveUserPushToken } = require("../../controllers/mobile-app/notifications");
+const { saveUserPushToken, sendPushNotification } = require("../../controllers/mobile-app/notifications");
 const router = express.Router();
 
 router.route("/token-save").post(saveUserPushToken);
+router.route("/send-push").post(sendPushNotification);
 
 module.exports = router;
