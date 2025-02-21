eae1cda3316b11620ae6d8160979728e23dad1d7
tatarynrm <tatarynrm@gmail.com>
2025-02-21 17:23:09 +0200

    ewqewq

--- telegram__bot/telegram_bot.js
+++ telegram__bot/telegram_bot.js
@@ -17,45 +17,45 @@ bot.start(async (ctx) => {
   
   await mainMenuFunc(ctx)
 
-  if (ctx.message.from.id === 941236974 || ctx.message.from.id === 282039969 || ctx.message.from.id === 526930289 || ctx.message.from.id === 5298432643 || ctx.message.from.id === 1905920358) {
-    await ctx.telegram.sendMessage(ctx.chat.id, "Головне меню", {
-      parse_mode: "html",
-      reply_markup: {
-        keyboard: [
-          [
-            { text: "Активні користувачі", callback_data: "activeUsers" },
-            { text: "Перезавантажити дані", callback_data: "reloadData" },
-          ],
-          [{ text: "Менеджери з логістики", callback_data: "allLogists" }],
-          [{ text: "Аналіз роботи відділів", callback_data: "viddilJob" }],
-          [
-            {
-              text: "Звіт по роботі менеджерів",
-              callback_data: "managersWorkData",
-            },
-          ],
-          [
-            {
-              text: "Нагадування:Актуальність заявок",
-              callback_data: "managerZapReminder",
-            },
-          ],
-        ],
-        resize_keyboard: true,
-      },
-    });
-  } else {
-    await ctx.telegram.sendMessage(ctx.chat.id, "Ви в режимі користувача.");
-    await ctx.reply("Функції користувача", {
-      reply_markup: {
-        keyboard: [
-          [{ text: "Моя експедиція" }],
-          [{ text: "Некомплект документів" }],
-        ],
-        resize_keyboard: true,
-      },
-    });
-  }
+  // if (ctx.message.from.id === 941236974 || ctx.message.from.id === 282039969 || ctx.message.from.id === 526930289 || ctx.message.from.id === 5298432643 || ctx.message.from.id === 1905920358) {
+  //   await ctx.telegram.sendMessage(ctx.chat.id, "Головне меню", {
+  //     parse_mode: "html",
+  //     reply_markup: {
+  //       keyboard: [
+  //         [
+  //           { text: "Активні користувачі", callback_data: "activeUsers" },
+  //           { text: "Перезавантажити дані", callback_data: "reloadData" },
+  //         ],
+  //         [{ text: "Менеджери з логістики", callback_data: "allLogists" }],
+  //         [{ text: "Аналіз роботи відділів", callback_data: "viddilJob" }],
+  //         [
+  //           {
+  //             text: "Звіт по роботі менеджерів",
+  //             callback_data: "managersWorkData",
+  //           },
+  //         ],
+  //         [
+  //           {
+  //             text: "Нагадування:Актуальність заявок",
+  //             callback_data: "managerZapReminder",
+  //           },
+  //         ],
+  //       ],
+  //       resize_keyboard: true,
+  //     },
+  //   });
+  // } else {
+  //   await ctx.telegram.sendMessage(ctx.chat.id, "Ви в режимі користувача.");
+  //   await ctx.reply("Функції користувача", {
+  //     reply_markup: {
+  //       keyboard: [
+  //         [{ text: "Моя експедиція" }],
+  //         [{ text: "Некомплект документів" }],
+  //       ],
+  //       resize_keyboard: true,
+  //     },
+  //   });
+  // }
 });
 
 bot.on('contact', async(ctx) => {
