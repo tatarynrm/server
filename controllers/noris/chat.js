f3e68d1af48acaa912e95fd8af9ae16d55a374e3
tatarynrm <tatarynrm@gmail.com>
2025-03-26 21:24:47 +0200

    qweweqweq

--- controllers/noris/chat.js
+++ controllers/noris/chat.js
@@ -1,8 +1,7 @@
 const { norisdb, ict_managers } = require("../../db/noris/noris");
 
 const getLastMessages = async (req, res) => {
-
-    const {room_id} = req.body;
+  const { room_id } = req.body;
   try {
     const result = await ict_managers.query(
       `
@@ -10,10 +9,9 @@ SELECT * FROM chat
 where deleted = false and room_id =$1
 ORDER BY created_at ASC
 LIMIT 100;
-         `,[room_id]
+         `,
+      [room_id]
     );
-
-
 
     if (result.rows) {
       res.json(result.rows);
@@ -23,50 +21,95 @@ LIMIT 100;
   }
 };
 const deleteOneMessage = async (req, res) => {
-    const { id } = req.body; // Отримуємо id повідомлення з тіла запиту
-    try {
-      const result = await ict_managers.query(
-        `
+  const { id } = req.body; // Отримуємо id повідомлення з тіла запиту
+  try {
+    const result = await ict_managers.query(
+      `
         UPDATE chat 
         SET deleted = true 
         WHERE id = $1
-        `, 
-        [id] // id повідомлення, яке потрібно позначити як видалене
-      );
-  
-      if (result.rowCount > 0) {
-        // Якщо запис був успішно оновлений
-        res.status(200).json({ message: 'Message marked as deleted' });
-      } else {
-        // Якщо запис не знайдений
-        res.status(404).json({ message: 'Message not found' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error deleting message' });
+        `,
+      [id] // id повідомлення, яке потрібно позначити як видалене
+    );
+
+    if (result.rowCount > 0) {
+      // Якщо запис був успішно оновлений
+      res.status(200).json({ message: "Message marked as deleted" });
+    } else {
+      // Якщо запис не знайдений
+      res.status(404).json({ message: "Message not found" });
     }
-  };
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting message" });
+  }
+};
 const getChatRooms = async (req, res) => {
-    
-    try {
-      const result = await ict_managers.query(
-        `
+  try {
+    const result = await ict_managers.query(
+      `
       select * from chat_rooms
-        ` 
-      );
-  
-res.json(result.rows)
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error deleting message' });
-    }
-  };
+        `
+    );
 
-  
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting message" });
+  }
+};
 
+const insertOrUpdateLastViewed = async (req, res) => {
+  const { user_id } = req.body;
+
+  try {
+    const query = `
+        INSERT INTO last_time_messages_view (user_id, last_viewed_at)
+        VALUES ($1, CURRENT_TIMESTAMP)
+        ON CONFLICT (user_id) DO UPDATE
+        SET last_viewed_at = CURRENT_TIMESTAMP
+        RETURNING user_id, last_viewed_at;
+      `;
+
+    const values = [user_id];
+
+    // Виконуємо запит
+    const result = await ict_managers.query(query, values);
+
+    // Повертаємо результат
+    res.json(result.rows[0]); // Повертаємо запис, що було вставлено або оновлено
+  } catch (err) {
+    console.error("Помилка при виконанні запиту:", err);
+    res.status(500).json({ error: "Помилка при виконанні запиту" });
+  }
+};
+
+const getCountOfUnreadMessages = async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const result = await ict_managers.query(
+      `
+            
+            SELECT COUNT(*) 
+FROM chat c
+WHERE c.created_at > (
+    SELECT ltmv.last_viewed_at 
+    FROM last_time_messages_view ltmv
+    WHERE ltmv.user_id = $1
+)`,
+      [user_id]
+    );
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   getLastMessages,
   deleteOneMessage,
-  getChatRooms
+  getChatRooms,
+  insertOrUpdateLastViewed,
+  getCountOfUnreadMessages,
 };
--- routes/noris/chat.js
+++ routes/noris/chat.js
@@ -1,12 +1,14 @@
 
 const express = require("express");
-const { getLastMessages, deleteOneMessage, getChatRooms } = require("../../controllers/noris/chat");
+const { getLastMessages, deleteOneMessage, getChatRooms, insertOrUpdateLastViewed, getCountOfUnreadMessages } = require("../../controllers/noris/chat");
 
 const router = express.Router();
 
 router.route("/last").post(getLastMessages);
 router.route("/delete-message").post(deleteOneMessage);
 router.route("/rooms").get(getChatRooms);
+router.route("/view-time").post(insertOrUpdateLastViewed);
+router.route("/unread").post(getCountOfUnreadMessages);
 
 
 
